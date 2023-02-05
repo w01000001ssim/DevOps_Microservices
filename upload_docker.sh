9952c36ac5ef51054f063e8fcb31dec7b65f8103
w01000001ssim <m.hadef@univ-soukahras.dz>
2023-02-05 21:02:41 +0100

    upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=w01000001ssim/ml-microservice
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username=w01000001ssim --password=3tWLjUqkagbeark
+docker tag ml-microservice $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
